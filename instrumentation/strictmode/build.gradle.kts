3df8e34fc55e20dafdedbaee9c98d665dc92210e
Hadi Satrio <hi@hadisatrio.com>
2024-08-15 12:29:30 +0700

    Add a new instrumentation module for StrictModˆe

---
+++ instrumentation/strictmode/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("otel.android-library-conventions")
+    id("otel.publish-conventions")
+}
+
+description = "OpenTelemetry Android StrictMode instrumentation"
+
+android {
+    namespace = "io.opentelemetry.android.instrumentation.strictmode"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+    api(platform(libs.opentelemetry.platform))
+    api(libs.opentelemetry.api)
+    api(project(":core"))
+    implementation(libs.androidx.core)
+    implementation(libs.opentelemetry.semconv.incubating)
+    implementation(libs.opentelemetry.sdk)
+    implementation(libs.opentelemetry.instrumentation.api)
+    testImplementation(libs.awaitility)
+    testImplementation(libs.robolectric)
+}
